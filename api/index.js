f1e651295726ddd6879e813fcaf66cee299ccbe1
MparOp <officialparvej.mo@gmail.com>
2025-04-17 12:57:38 +0530

    modified index js and vercel json

--- api/index.js
+++ api/index.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 var cors = require('cors')
 app.use(cors())
+const serverless = require('serverless-http');
 
 const bodyParser = require('body-parser');
-const userRoutes = require("./route/User");
-const locationRoutes = require('./route/location-routes');
+const userRoutes = require("../route/User");
+const locationRoutes = require('../route/location-routes');
 
 
 const cookieParser = require("cookie-parser");                                               //fronend ki request enterten karenga bakend me using cors
-const {cloudinaryConnect} = require("./config/cloudinary");
+const {cloudinaryConnect} = require("../config/cloudinary");
 cloudinaryConnect();
 
 
@@ -25,7 +26,7 @@ app.use(fileUpload({
 
 require('dotenv').config();
 const PORT=process.env.PORT || 8000;   
-require("./config/dataBase").connect();
+require("../config/dataBase").connect();
 
 
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -52,8 +53,8 @@ app.use(cookieParser());
 // )
 
 
-const Upload = require("./route/fileUpload")
-const College = require('./route/college-routes')
+const Upload = require("../route/fileUpload")
+const College = require('../route/college-routes')
 
 
 //routes
@@ -72,7 +73,9 @@ app.get("/",(req,res)=>{
     })
 })
 
-//activate......
+// activate......
 app.listen(PORT , () => {
     console.log(`app listening on port  ${PORT}`) ;
 })
+
+module.exports = serverless(app);
